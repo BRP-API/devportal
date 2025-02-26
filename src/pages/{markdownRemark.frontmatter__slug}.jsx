4c53db0b7631b4293b089ff61d75c0bd745b599d
nkorporaal <nap.korporaal+github@gmail.com>
2025-02-26 08:42:25 +0100

    Updated styles

--- src/pages/{markdownRemark.frontmatter__slug}.jsx
+++ src/pages/{markdownRemark.frontmatter__slug}.jsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React, { useState } from 'react';
 import { graphql } from "gatsby"
 import './styles.css';
 import bewoningConfig from "../content/bewoning/_config.yaml"
@@ -6,44 +6,81 @@ import personenConfig from "../content/personen/_config.yaml"
 import centralLogoImage from '../img/logo.svg';
 
 // import rijkshuisstijl componenten en css classes
-import { Footer, ColumnLayout, LinkList, LinkListLink, Icon, NavBar, Heading, LinkListCard, Separator } from '@rijkshuisstijl-community/components-react'
+import { Footer, ColumnLayout, LinkList, LinkListLink, Icon, NavBar, Heading, LinkListCard, Separator, IconButton } from '@rijkshuisstijl-community/components-react'
 import './rhc-styles.css';
 
 // import redoc standalone component
 import { RedocStandalone } from "redoc";
 
-const Header = ({ header }) => (
-  <header className="header">
-    <div className="header-top">
-      <img src={centralLogoImage} alt="central-logo" className="central-logo" />
-      <span className="header-logo-container">
-        <Heading appearanceLevel={3} level={1}>
-          {header.title}
-        </Heading>
-      </span>
-    </div>
-    <Separator />
-    <div>
-      <NavBar className="header-navbar"
-        headingItem={{
-          href: 'https://brp-api.github.io/devportal/',
-          id: 'home',
-          label: 'Home'
-        }}
-        items={header.navbar.items}
-      />
-    </div>
-  </header>
-);
+const Header = ({ header }) => {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
+
+  return (
+    <header className="header">
+      <div className="header-top">
+        <img src={centralLogoImage} alt="central-logo" className="central-logo" />
+        <span className="header-logo-container">
+          <Heading appearanceLevel={4} level={1}>
+            {header.title}
+          </Heading>
+        </span>
+      </div>
+      <Separator />
+      <div className="navbar desktop">
+        <NavBar className="header-navbar"
+          headingItem={header.navbar.headingItem}
+          items={header.navbar.items}
+        />
+      </div>
+      <div className="navbar-mobile">
+        <IconButton label="menu" onClick={toggleMenu}>
+          <Icon icon="menu" />
+        </IconButton>
+        <NavBar className="header-navbar"
+          headingItem={header.navbar.headingItem}
+          items={[]}
+        />
+      </div>
+      {isMenuVisible && (
+        <div className="mobile-menu">
+          <IconButton label="kruis" onClick={toggleMenu}>
+            <Icon icon="kruis" />
+          </IconButton>
+          <LinkListCard>
+            <LinkList>
+              {header.navbar.items.map((item) => (
+                <LinkListLink key={item.id} href={item.href}
+                  target={item.blank ? '_blank' : ''}
+                  rel={item.blank ? 'noopener noreferrer' : ''}
+                  icon={<Icon icon={item.blank ? "externe-link" : "chevron-right"} />}>
+                  {item.label}
+                </LinkListLink>
+              ))}
+            </LinkList>
+          </LinkListCard>
+
+
+        </div>
+      )}
+    </header>
+  );
+};
 
 const Sidebar = ({ nav }) => (
-  <nav className="sidebar">
+  <nav className="sidebar desktop">
     {nav.map((item, index) => (
       <div key={index}>
         <LinkListCard headingLevel={3} heading={item.title}>
           <LinkList>
             {item.subnav.map((subitem, subindex) => (
-              <LinkListLink key={subindex} href={subitem.href} icon={<Icon icon="chevron-right" />}>
+              <LinkListLink key={subindex} href={subitem.href}
+                target={subitem.blank ? '_blank' : ''}
+                rel={subitem.blank ? 'noopener noreferrer' : ''}
+                icon={<Icon icon={subitem.blank ? "externe-link" : "chevron-right"} />}>
                 {subitem.label}
               </LinkListLink>
             ))}
