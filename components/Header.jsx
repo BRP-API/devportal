a4549902d670fe784a4b6251c6935110ed74b05e
Niels Korporaal <nkorporaal>
2025-03-20 14:17:34 +0100

    gebruik env variabele

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import Image from 'next/image';
-import getConfig from 'next/config';
 
 const Header = () => {
-  const { publicRuntimeConfig } = getConfig();
-  const basePath = publicRuntimeConfig.basePath || '';
-
+  const basePath = process.env.NODE_ENV === 'production' ? '/devportal' : '';
+  
   return (
     <header className='header'>
       <Image src={`${basePath}/logo.svg`} alt="central-logo" className="central-logo" width={60} height={100} />
