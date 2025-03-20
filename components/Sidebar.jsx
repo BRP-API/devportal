2b715fb1d518e5e35dc7fdf1693548d37432edcf
Niels Korporaal <nkorporaal>
2025-03-20 16:34:48 +0100

    mobile support

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Link from 'next/link';
 
 const Sidebar = ({ sidebar }) => {
+  const basePath = process.env.NODE_ENV === 'production' ? '/devportal' : '';
+
   const defaultSidebar = {
     items: [
       {
@@ -70,40 +72,84 @@ const Sidebar = ({ sidebar }) => {
   const sidebarData = sidebar || defaultSidebar;
 
   return (
-    <div className="sidebar">
-      <div>
-        {sidebarData.items.map((item, index) => (
-          <div key={index}>
-            {/* display title label */}
-            {item.title && <p className="item title">{item.title}</p>}
-            {item.subnav.map((subitem, subindex) => (
-              <div key={subindex}>
-                {subitem.href && (
-                  <Link className="subitem" href={subitem.href}>
-                    {subitem.label}
-                  </Link>
-                )}
-                {/* display title only */}
-                {!subitem.href && subitem.label && (
-                  <p className="subitem">{subitem.label}</p>
-                )}
-                {/* display subnav with multiple items */}
-                {subitem.title && (
-                  <details className="details-flex">
-                    <summary>{subitem.title}</summary>
-                    {subitem.subnav.map((subsubitem, index) => (
-                      <Link className="subsubitem" href={subsubitem.href} key={index}>
-                        {subsubitem.label}
+    <>
+      <div className="sidebar desktop">
+        <div>
+          {sidebarData.items.map((item, index) => (
+            <div key={index}>
+              {/* display title label */}
+              {item.title && <p className="item title">{item.title}</p>}
+              {item.subnav.map((subitem, subindex) => (
+                <div key={subindex}>
+                  {subitem.href && (
+                    <Link className="subitem" href={subitem.href}>
+                      {subitem.label}
+                    </Link>
+                  )}
+                  {/* display title only */}
+                  {!subitem.href && subitem.label && (
+                    <p className="subitem">{subitem.label}</p>
+                  )}
+                  {/* display subnav with multiple items */}
+                  {subitem.title && (
+                    <details className="details-flex">
+                      <summary>{subitem.title}</summary>
+                      {subitem.subnav.map((subsubitem, index) => (
+                        <Link className="subsubitem" href={subsubitem.href} key={index}>
+                          {subsubitem.label}
+                        </Link>
+                      ))}
+                    </details>
+                  )}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="sidebar-mobile">
+        <span className='menu-toggle' onClick={() => document.querySelector('.mobile-menu').classList.add('open')}>
+          <img src={`${basePath}/menu.svg`} alt="menu" />
+          Menu
+        </span>
+
+        <div className="mobile-menu">
+          <div className='menu-toggle' onClick={() => document.querySelector('.mobile-menu').classList.remove('open')}>
+            <img src={`${basePath}/close.svg`} alt="menu-close" />
+          </div>
+          <div className='sidebar'>
+            {sidebarData.items.map((item, index) => (
+              <div key={index}>
+                {item.title && <p className="item title">{item.title}</p>}
+                {item.subnav.map((subitem, subindex) => (
+                  <div key={subindex}>
+                    {subitem.href && (
+                      <Link className="subitem" href={subitem.href} onClick={() => document.querySelector('.mobile-menu').classList.remove('open')}>
+                        {subitem.label}
                       </Link>
-                    ))}
-                  </details>
-                )}
+                    )}
+                    {!subitem.href && subitem.label && (
+                      <p className="subitem">{subitem.label}</p>
+                    )}
+                    {subitem.title && (
+                      <details className="details-flex">
+                        <summary>{subitem.title}</summary>
+                        {subitem.subnav.map((subsubitem, index) => (
+                          <Link className="subsubitem" href={subsubitem.href} key={index} onClick={() => document.querySelector('.mobile-menu').classList.remove('open')}>
+                            {subsubitem.label}
+                          </Link>
+                        ))}
+                      </details>
+                    )}
+                  </div>
+                ))}
               </div>
             ))}
           </div>
-        ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
