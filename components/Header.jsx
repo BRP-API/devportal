e481617233dc426e03a5fe95770c3ae6ce833e71
nkorporaal <nap.korporaal+github@gmail.com>
2025-04-09 13:19:06 +0200

    Added huisstijl

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,16 +3,27 @@ import Image from 'next/image';
 
 const Header = () => {
   const basePath = process.env.NODE_ENV === 'production' ? '/devportal' : '';
-  
+
   return (
-    <header className='header'>
-      <Image src={`${basePath}/logo.svg`} alt="central-logo" className="central-logo" width={60} height={100} />
-      <div className="header-top">
-        <div className="header-title">
-          <h1>BRP API</h1>
+    <>
+      <header className='header'>
+        <a href="/" title="Naar de homepage van BRP API devportal">
+          <div className="logo">
+            <div className="wrapper">
+              <img src={`${basePath}/logo-ri.svg`} alt="central-logo-desktop" className="desktop" />
+              <Image src={`${basePath}/logo.svg`} alt="central-logo-mobile" className="mobile banner" height={100} width={80} />
+            </div>
+          </div>
+        </a>
+      </header>
+      <div id="navbar">
+        <div className="wrapper">
+          <nav>
+            <a className="title">BRP API</a>
+          </nav>
         </div>
       </div>
-    </header>
+    </>
   );
 };
 
