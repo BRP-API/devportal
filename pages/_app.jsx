3a62cea3e09bf591c2147cb001e0436475082443
nkorporaal <nap.korporaal+github@gmail.com>
2025-05-19 16:06:46 +0200

    fix favicon basePath issue

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,11 +3,13 @@ import '@/styles/globals.css'
 import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
+  const basePath = process.env.NODE_ENV === 'production' ? '/devportal' : '';
+
   return <>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content="BRP API Developer Portal" />
-      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" href={`${basePath}/favicon.ico`} sizes="any" />
       <title>BRP API Developer Portal</title>
     </Head>
     <Layout>
